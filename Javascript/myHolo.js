a1322a0fb1b18ef3dd42aba6202fccfc8e5cc15e
SeowonC <21changs@ptsdwebapps.com>
2019-03-08 21:42:33 -0500

    Amp viz with sphere

--- Javascript/myHolo.js
+++ Javascript/myHolo.js
@@ -1,13 +1,17 @@
 var sound, amplitude, cnv;
 var fft, peakDetect;
-var ellipseWidth = 0;
+var peakSize;
+var peakColor;
+var width = 800;
+var height = 800;
+
 
 function preload(){
   sound = loadSound('Audio/Italy_Jr.mp3');
 }
 
 function setup(){
-  cnv = createCanvas(700,700);
+  cnv = createCanvas(800,800, WEBGL);
   cnv.mouseClicked(togglePlay);
   fft = new p5.FFT();
   sound.amp(0.2);
@@ -24,44 +28,36 @@ function setup(){
   text('click to play', width/2, height/2);
 }
 
+
 function draw(){
-  background(0);
-    
-  var spectrum = fft.analyze();
-  noStroke();
-  fill(0,0,0); // spectrum is green
-  for (var i = 0; i< spectrum.length; i++){
-    var x = map(i, 0, spectrum.length, 0, width);
-    var h = -height + map(spectrum[i], 0, 255, height, 0);
-    rect(x, height, width / spectrum.length, h )
-  }
-
-  var waveform = fft.waveform();
-  noFill();
-  beginShape();
-  stroke(0,0,0); // waveform is red
-  strokeWeight(1);
-  for (var i = 0; i< waveform.length; i++){
-    var x = map(i, 0, waveform.length, 0, width);
-    var y = map( waveform[i], -1, 1, 0, height);
-    vertex(x,y);
-  }
-  endShape();
-    
+  background(0); 
   var level = amplitude.getLevel();
-  var size = map(level, 0, 1, 0, 1000);
-  stroke(0,0,255);
-  ellipse(width/2, height/2, size*(Math.random()+2), size*(Math.random()+2));
-
+  var size = map(level, 0, 1, 0, 800);
+  stroke(0,0,0);
+  fill(0,255,0);
+   background(0); {
+        rotateX(frameCount * 0.005);
+        rotateZ(frameCount * 0.005);
+        sphere(size,24,16);
+        fill(255,0,0,150);
+   }
+    
   fft.analyze();
   peakDetect.update(fft);
-    
-  stroke(0,255,255);
-  ellipseWidth *= 0.95;
-  ellipse(width/2, height/2, ellipseWidth*2, ellipseWidth*2);
-    
+  fill(0,0,peakColor);
+  stroke(0,0,peakColor);
+  peakColor = peakColor*.975;
+  peakSize = peakSize*1.03;
+  ellipse(width/1000, height/1000, peakSize, peakSize);
+  if (peakSize < 900) {
+      peakSize = peakSize;
+  } else {
+      peakSize = 0;
+  }
+ 
   
 }
+
 
 // fade sound if mouse is over canvas
 function togglePlay() {
@@ -76,7 +72,11 @@ function togglePlay() {
 
 // this function is called by peakDetect.onPeak
 function triggerBeat() {
-  ellipseWidth = 50;
+  peakSize = 100;
+  peakColor = 255;
+  console.log ("Check");
+  console.log (peakSize);
+
 }
 
 // mouseclick starts/stops sound
